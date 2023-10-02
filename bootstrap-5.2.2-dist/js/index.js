e6683223e4eea3e357104f11b8f90ae21c44193b
unknown <momen07996@gmail.com>
2023-10-02 14:49:02 +0300

    start sections carousel and team

--- bootstrap-5.2.2-dist/js/index.js
+++ bootstrap-5.2.2-dist/js/index.js
@@ -35,3 +35,17 @@ window.addEventListener("scroll", function () {
     section.classList.add("d-none");
   }
 });
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
